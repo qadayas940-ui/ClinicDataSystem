b175df885f3f92c3b567e1003209a3db283b50d6
qadayas940-ui <qadayas940@gmail.com>
2026-09-16 20:20:59 +0300

    Update config/settings/base.py for production 1.2.0

--- config/settings/base.py
+++ config/settings/base.py
@@ -235,8 +235,8 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # معلومات التطبيق
 # ---------------------------------------------------------------------------
 APP_NAME = "نظام إدارة بيانات ومرضى العيادة"
-APP_VERSION = "1.1.0"
-DB_SCHEMA_VERSION = "1"
+APP_VERSION = "1.2.0"
+DB_SCHEMA_VERSION = "2"
 FACILITY_NAME = config("FACILITY_NAME", default="عيادة الموصل الخيرية")
 UPDATE_MANIFEST_URL = config("UPDATE_MANIFEST_URL", default="")
 
